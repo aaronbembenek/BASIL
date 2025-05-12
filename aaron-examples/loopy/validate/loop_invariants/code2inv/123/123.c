5b8eb9c1ee5a019214afefda43d2a36bdb5cb44b
Aaron Bembenek <aaron.bembenek@gmail.com>
2025-05-13 09:28:47 +1000

    Update C files for Ultimate.

--- aaron-examples/loopy/validate/loop_invariants/code2inv/123/123.c
+++ aaron-examples/loopy/validate/loop_invariants/code2inv/123/123.c
@@ -3,11 +3,31 @@
 #define assume(e)                                                              \
   if (!(e))                                                                    \
     exit(-1);
+#ifdef __BASIL__
 int __attribute__((noinline)) my_unknown() { return rand(); }
 void __attribute__((noinline)) my_assert(int x) {
   while (!x)
     ;
 }
+#else
+extern void __assert_fail(const char *, const char *, unsigned int,
+                          const char *) __attribute__((__nothrow__, __leaf__))
+__attribute__((__noreturn__));
+void reach_error() { __assert_fail("0", "benchmark", 3, "reach_error"); }
+
+extern int __VERIFIER_nondet_int();
+void __VERIFIER_assert(int cond) {
+  if (!(cond))
+  ERROR: {
+    reach_error();
+    abort();
+  }
+    return;
+}
+
+#define my_unknown() __VERIFIER_non_det_int()
+#define my_assert(x) __VERIFIER_assert(x)
+#endif
 int main() {
   int i = (int)my_unknown();
   int size = (int)my_unknown();
